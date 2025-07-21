4ddebdf6f1ebb2daf1e6b3ecd955ff307786bb86
Ibrahim H. Yazouri <ibrahimhyaz@gmail.com>
2025-07-21 11:03:44 +0200

    build ☕: generate unique username after registeration

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,10 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 

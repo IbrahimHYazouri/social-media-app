7e421ec14cf57a422e8fe43408306c1f0cc1ec12
Ibrahim H. Yazouri <ibrahimhyaz@gmail.com>
2025-07-27 13:54:44 +0200

    refactor: redifine the relations between post & eaction models to polymorphic relations

--- app/Models/Reaction.php
+++ app/Models/Reaction.php
@@ -4,15 +4,35 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
+/**
+ * @property-read int $id
+ * @property-read int $object_id
+ * @property-read string $object_type
+ * @property-read int $user_id
+ * @property-read string $type
+ * @property-read DateTimeInterface $created_at
+ * @property-read DateTimeInterface $updated_at
+ */
 final class Reaction extends Model
 {
-    protected $fillable = ['user_id', 'post_id', 'type'];
+    protected $fillable = ['user_id', 'object_id', 'object_type', 'type'];
 
-    public function post(): BelongsTo
+    public function reactable(): MorphTo
     {
-        return $this->belongsTo(Post::class);
+        return $this->morphTo(
+            'reactable',
+            'object_type',
+            'object_id',
+        );
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }

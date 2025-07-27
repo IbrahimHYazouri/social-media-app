a2d811aafdbcd79d084d0583098f14d2e68a8a3e
Ibrahim H. Yazouri <ibrahimhyaz@gmail.com>
2025-07-27 13:54:03 +0200

    refactor: update the reactions table to be polymorphic to use it for both post & comment reactions

---
+++ database/migrations/2025_07_27_114045_convert_reactions_table_to_polymorphic.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reactions', function (Blueprint $table) {
+            $table->dropForeign('reactions_post_id_foreign');
+            $table->renameColumn('post_id', 'object_id');
+            $table->string('object_type')->after('object_id');
+        });
+
+        DB::table('reactions')->update(['object_type' => \App\Models\Post::class]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reactions', function (Blueprint $table) {
+            //
+        });
+    }
+};

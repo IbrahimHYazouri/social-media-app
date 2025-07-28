b334db74179c0ce8787e93f711f44c7347c58bfc
Ibrahim H. Yazouri <ibrahimhyaz@gmail.com>
2025-07-28 12:00:47 +0200

    build: handle adding reaction to comments

--- app/Http/Controllers/ReactionController.php
+++ app/Http/Controllers/ReactionController.php
@@ -5,17 +5,24 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreReactionRequest;
+use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 
 final class ReactionController extends Controller
 {
-    public function __invoke(StoreReactionRequest $request, Post $post): JsonResponse
+    public function __invoke(StoreReactionRequest $request, ?Post $post = null, ?Comment $comment = null): JsonResponse
     {
-        $data = $request->validated();
+        $reactable = $post ?? $comment;
+
+        if (! $reactable) {
+            abort(404);
+        }
+
         $user = Auth::user();
-        $existing = $post->reactions()
+
+        $existing = $reactable->reactions()
             ->where('user_id', $user->id)
             ->first();
 
@@ -23,16 +30,16 @@ final class ReactionController extends Controller
             $existing->delete();
             $hasReaction = false;
         } else {
-            $post->reactions()->create([
+            $reactable->reactions()->create([
                 'user_id' => $user->id,
-                'type' => $data['type'],
+                'type' => $request->validated('type'),
             ]);
             $hasReaction = true;
         }
 
         return response()->json([
-            'num_of_reactions' => $post->reactions()->count(),
-            'current_user_has_reaction' => $hasReaction,
+            'num_of_reactions' => $reactable->reactions()->count(),
+            'user_has_reacted' => $hasReaction,
         ]);
     }
 }

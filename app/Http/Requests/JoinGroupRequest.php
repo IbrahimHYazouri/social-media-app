1d3a7785e1e9b33de4507537c5cc48a2067f6a43
Ibrahim H. Yazouri <ibrahimhyaz@gmail.com>
2025-08-01 13:52:59 +0200

    build: handle joining group for members

---
+++ app/Http/Requests/JoinGroupRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use App\Models\GroupUser;
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+final class JoinGroupRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        /**
+         * @var User $user
+         */
+        $user = Auth::user();
+        $group = $this->route('group');
+
+        $already = GroupUser::where([
+            'group_id' => $group->id,
+            'user_id' => $user->id,
+        ])->exists();
+
+        return $user && !$already;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}

c6fe340724ea6e5b219ca42e6a8cf422cddd5fa3
Ibrahim H. Yazouri <ibrahimhyaz@gmail.com>
2025-08-03 13:27:55 +0200

    fix: redirect back after Join, Invite request instead of rendering the Inertia page again

--- app/Http/Controllers/JoinGroupController.php
+++ app/Http/Controllers/JoinGroupController.php
@@ -7,21 +7,19 @@ namespace App\Http\Controllers;
 use App\Enums\GroupUserRoleEnum;
 use App\Enums\GroupUserStatusEnum;
 use App\Http\Requests\JoinGroupRequest;
-use App\Http\Resources\GroupResource;
 use App\Models\Group;
 use App\Models\GroupUser;
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Inertia\Inertia;
-use Inertia\Response;
 
 final class JoinGroupController extends Controller
 {
     /**
      * Handle incoming request
      */
-    public function __invoke(JoinGroupRequest $request, Group $group): Response
+    public function __invoke(JoinGroupRequest $request, Group $group): RedirectResponse
     {
         /**
          * @var User $user
@@ -46,9 +44,6 @@ final class JoinGroupController extends Controller
             'owner_id' => $group->user_id,
         ]);
 
-        return Inertia::render('Group/Show', [
-            'success' => $message,
-            'group' => GroupResource::make($group),
-        ]);
+        return back()->with('success', $message);
     }
 }

52d38bd755a66fbaaa4bfb1738bf6350072a721b
Ibrahim H. Yazouri <ibrahimhyaz@gmail.com>
2025-08-19 11:38:20 +0200

    build: handle follow, unfollow user in backend

---
+++ app/Http/Controllers/FollowController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+final class FollowController extends Controller
+{
+    public function store(User $user)
+    {
+        /**
+         * @var User $authUser
+         */
+        $authUser = Auth::user();
+
+        $authUser->following()->syncWithoutDetaching($user->id);
+
+        return back();
+    }
+
+    public function destroy(User $user)
+    {
+        /**
+         * @var User $authUser
+         */
+        $authUser = Auth::user();
+
+        $authUser->following()->detach($user->id);
+
+        return back();
+    }
+}
